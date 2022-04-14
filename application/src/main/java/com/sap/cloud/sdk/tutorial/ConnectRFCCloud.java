4803a5508203e92aaa6ae5badeb45d21fd1d5696
FatihAksoy530 <fatihaksoy1999@hotmail.com>
2022-04-14 14:14:24 +0300

    first commit

---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/ConnectRFCCloud.java
@@ -0,0 +1,66 @@
+package com.sap.cloud.sdk.tutorial;
+
+import com.google.gson.Gson;
+import com.sap.cloud.sdk.cloudplatform.connectivity.Destination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
+import com.sap.cloud.sdk.s4hana.connectivity.exception.RequestExecutionException;
+import com.sap.cloud.sdk.s4hana.connectivity.rfc.RfmRequest;
+import com.sap.cloud.sdk.s4hana.connectivity.rfc.RfmRequestResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+
+ * Sample application that uses the Connectivity
+ service. In particular, it is
+
+ * making use of the capability to invoke a function module in an ABAP system
+
+ * via RFC
+
+ *
+
+ * Note: The JCo APIs are available under <code>com.sap.conn.jco</code>.
+
+ */
+
+
+@WebServlet("/ConnectivityRFCCloud")
+public class ConnectRFCCloud extends HttpServlet {
+    public static final long serialVersionUID = 1L;
+    public static final Logger logger = LoggerFactory.getLogger(ConnectRFCCloud.class);
+    public static final Destination destinationRfc =
+            DestinationAccessor.getDestination("sapuretestrfc");
+
+    @Override
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException {
+
+        logger.info("Start get method: " + request.getRequestURI());
+        String parameter = request.getParameter("name");
+        logger.info("Get parameter 'name': " + parameter);
+        if (parameter == null) {
+            parameter = "STFC_CONNECTION";
+        }
+        Iterable names = destinationRfc.getPropertyNames();
+        logger.info(new Gson().toJson(names));
+
+        try {
+            final RfmRequestResult rfmTest = new RfmRequest(parameter, false).withChanging("REQUTEXT" ,"STRING", "JCO successful").execute(destinationRfc); //false is for non-commit
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(new Gson().toJson(rfmTest));
+        } catch (RequestExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package com.sap.cloud.sdk.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet
+{
+    public static final long serialVersionUID = 1L;
+    public static final Logger logger = LoggerFactory.getLogger(HelloWorldServlet.class);
+
+    @Override
+    public void doGet( final HttpServletRequest request, final HttpServletResponse response )
+        throws IOException
+    {
+        logger.info("I am running!");
+        response.getWriter().write("Hello World extra!");
+    }
+}
